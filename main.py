f3d27d9f2b378823d9f76a5db0b93134b1cac469
Anita Orstad <anita.orstad@gmail.com>
2025-07-29 21:25:37 +0200

    added a test branch, mode and test bot

--- main.py
+++ main.py
@@ -9,19 +9,29 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Retrieve token from the .env file
+load_dotenv()
+env = os.getenv("ENVIRONMENT")
+if env == "TEST":
+    token = os.getenv("TEST_TOKEN")
+else:
+    token = os.getenv("MAIN_TOKEN") 
+
 # Set the confirmation message when the bot is ready
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {bot.user.name} in {env}-mode.')
+
 # Set the commands for your bot
 @bot.command()
-async def greet(ctx):
-    response = 'Hello, I am your discord bot'
+async def list_command(ctx):
+    cmds = [f"!{cmd}" for cmd in bot.commands]
+    response = "You can use the following commands:\n" + "\n".join(cmds)
     await ctx.send(response)
 
 @bot.command()
-async def list_command(ctx):
-    response = 'You can use the following commands: \n !greet \n !list_command \n !functions'
+async def greet(ctx):
+    response = 'Hello, I am your discord bot'
     await ctx.send(response)
 
 @bot.command()
@@ -29,6 +39,5 @@ async def functions(ctx):
     response = 'I am a simple Discord chatbot! I will reply to your command!'
     await ctx.send(response)
 
-# Retrieve token from the .env file
-load_dotenv()
-bot.run(os.getenv('TOKEN'))
+
+bot.run(token)
