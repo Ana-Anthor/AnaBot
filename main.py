9b5b5bfbd8f7fd83883f80af307e79ae7762d887
Anita Orstad <anita.orstad@gmail.com>
2025-07-31 21:24:13 +0200

    Refacturated onboarding. Added bot commands

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import os
 from discord.ext import commands 
 from dotenv import load_dotenv
 
+from onboarding import handle_member_join, handle_onboarding_message
+
 # Create a Discord client instance and set the command prefix
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Retrieve token from the .env file
@@ -29,95 +30,13 @@ onboarding_roles = {}
 # When a member joins
 @bot.event
 async def on_member_join(member):
-    try:
-        member = await member.guild.fetch_member(member.id)
-    except discord.NotFound:
-        print("Member not found.")
-    
-    # Waiting for discord to finish setting up onboarding roles
-    await asyncio.sleep(10)
-    member = await member.guild.fetch_member(member.id)
-    print(f"{member.name} has these roles: {[role.name for role in member.roles]}")
-
-    # Save current roles from onboarding, eksclude @everyone
-    original_roles = [role for role in member.roles if role.name != "@everyone"]
-
-    # Stor in dictionary with user-ID as key
-    onboarding_roles[member.id] = original_roles
-
-    # Remove old roles
-    for role in original_roles:
-        try:
-            await member.remove_roles(role)
-            print(f"Removed role: {role.name}")
-        except Exception as e:
-            print(f"Could not remove role {role.name}: {e}")
-
-    # See if the server has a role called "New"
-    new_role = discord.utils.get(member.guild.roles, name="New")
-
-    # If not, make the role
-    if new_role is None:
-        new_role = await member.guild.create_role(name="New", colour=discord.Colour.orange())
-
-    # Give the New role to the new member
-    await member.add_roles(new_role)
-    member = await member.guild.fetch_member(member.id)
-    print(f"{member.name} has these roles: {[role.name for role in member.roles]}")
+    await handle_member_join(member)
 
 #When a member sends a message in waiting hall
 @bot.event
 async def on_message(message):
-    # Check if its a bot
-    if message.author.bot:
-        return 
-
-    #channels
-    welcome_channel = message.guild.get_channel(825128605868490815)
-    waiting_hall_id = 1399070297365155850
-    introduction_channel_id = 1151609579000561776
-    information_channel_id = 1363917217309130963
-    rules_channel_id = 1135973867001753711
-    help_and_questions_channel_id = 1397028615446855780
-
-    # Restor the roles from onboarding
-    if message.channel.id == waiting_hall_id:
-        member = message.author
-        server_name = member.guild.name
-
-        if member.id in onboarding_roles:
-            roles_to_restore = onboarding_roles[member.id]
-            for role in roles_to_restore:
-                await member.add_roles(role)
-            
-            # Remove the New role
-            new_role = discord.utils.get(member.guild.roles, name="New")
-            if new_role and new_role in member.roles:
-                await member.remove_roles(new_role)
-
-            onboarding_roles.pop(member.id, None)
-            
-            await message.channel.send(f"{member.mention}, you're officially a part of the squad! 🔓 You now have full access. Enjoy your stay!")
-
-            # Add the Member role
-            member_role = discord.utils.get(member.guild.roles, name="Member")
-            if member_role:
-                await member.add_roles(member_role)
-            else:
-                print("Member role not found.")
-
-
-            # Send a welcome message in the welcome channel
-            if welcome_channel:
-                await welcome_channel.send(f"Welcome to {server_name}, {member.mention}! "
-                                f"You’re now part of our awesome, chill gaming crew. 🎮 "
-                                f"Drop by <#{introduction_channel_id}> and tell us about yourself. We love meeting our squad! "
-                                f"Check <#{information_channel_id}> for server details and swing by <#{rules_channel_id}> to know what’s up. "
-                                f"Got questions? Hit <#{help_and_questions_channel_id}>, and staff or members will help. "
-                                f"If you spot us in voice chat, come say hi. We’d love to get to know you and game together!")
-
-    await bot.process_commands(message)
-
+    await handle_onboarding_message(message,bot)
+    
 # Set the commands for your bot
 @bot.command()
 async def list_command(ctx):
@@ -134,5 +53,16 @@ async def greet(ctx):
 async def functions(ctx):
     response = 'I am a simple Discord chatbot! I will reply to your command!'
     await ctx.send(response)
+
+@bot.command()
+async def serverinfo(ctx):
+    guild = ctx.guild
+    response = f"Server name: {guild.name}\nTotal members: {guild.member_count}"
+    await ctx.send(response)
+
+@bot.command()
+async def myroles(ctx):
+    roles = [role.name for role in ctx.author.roles if role.name != "@everyone"]
+    await ctx.send(f"Your roles: {', '.join(roles)}")
 
 bot.run(token)
---
+++ onboarding.py
@@ -0,0 +1,129 @@
+import discord
+import asyncio
+from discord.utils import get
+
+onboarding_roles = {}
+
+async def handle_member_join(member):
+    # Check if its a bot
+    if member.bot:
+        return 
+    
+    try:
+        member = await member.guild.fetch_member(member.id)
+    except discord.NotFound:
+        print("Member not found.")
+        return
+    
+    manager = OnboardingManager(member.guild)
+
+    # Waiting for discord to finish setting up onboarding roles
+    await asyncio.sleep(10)
+    member = await member.guild.fetch_member(member.id)
+    print(f"{member.name} has these roles: {[role.name for role in member.roles]}")
+
+    # Store member roles in dictionary with user-ID as key
+    original_roles = manager.get_roles(member)
+    onboarding_roles[member.id] = original_roles
+
+    # Remove old roles and add the role New
+    await manager.remove_all_roles(member, original_roles)
+    await manager.add_role_new(member)
+
+    member = await member.guild.fetch_member(member.id)
+    print(f"{member.name} has these roles: {[role.name for role in member.roles]}")
+
+async def handle_onboarding_message(message, bot):
+    # Check if its a bot
+    if message.author.bot:
+        return 
+    
+    waiting_hall_id = 1399070297365155850
+    welcome_channel = message.guild.get_channel(825128605868490815)
+    member = message.author
+    messageManager = WelcomeMessageManager(member)
+    manager = OnboardingManager(member.guild)
+
+    # Restor the roles from onboarding
+    if message.channel.id == waiting_hall_id:
+
+        if member.id in onboarding_roles:
+            await manager.restore_onboarding_roles(member)
+        
+            # Remove the New role
+            await manager.remove_role_new(member)
+            
+            await message.channel.send(f"{member.mention}, you're officially a part of the squad! 🔓 You now have full access. Enjoy your stay!")
+
+            # Add the Member role
+            await manager.add_role_member(member)
+            await messageManager.send_welcome_message(member, welcome_channel)
+
+    await bot.process_commands(message)
+
+class OnboardingManager:
+    def __init__(self, guild):
+        self.guild = guild
+
+    def get_roles(self, member):
+        return [role for role in member.roles if role.name != "@everyone"]
+
+    async def remove_all_roles(self, member, roles):
+        for role in roles:
+            try:
+                await member.remove_roles(role)
+                print(f"Removed role: {role.name}")
+            except Exception as e:
+                print(f"Error removing role {role.name}: {e}")
+
+    async def restore_onboarding_roles(self, member):
+        roles_to_restore = onboarding_roles[member.id, []]
+        for role in roles_to_restore:
+            try:
+                await member.add_roles(role)
+            except Exception as e:
+                print(f"Restoring onboarding roles for {member.mention} failed: {e}")
+
+
+    async def add_role_new(self, member):
+        # See if the server has a role called "New"
+        new_role = discord.utils.get(self.guild.roles, name="New")
+        # If not, make the role
+        if new_role is None:
+            new_role = await self.guild.create_role(name="New", colour=discord.Colour.orange())
+        # Give the New role to the new member
+        await member.add_roles(new_role)
+    
+    async def remove_role_new(self, member):
+        new_role = discord.utils.get(member.guild.roles, name="New")
+        if new_role and new_role in member.roles:
+            await member.remove_roles(new_role)
+
+            onboarding_roles.pop(member.id, None)
+
+    async def add_role_member(self, member):
+        member_role = discord.utils.get(member.guild.roles, name="Member")
+        if member_role:
+            await member.add_roles(member_role)
+        else:
+            print("Member role not found.")
+
+class WelcomeMessageManager:
+    def __init__(self, member):
+        self.member = member
+        self.server_name = member.guild.name
+        self.introduction_channel_id = 1151609579000561776
+        self.information_channel_id = 1363917217309130963
+        self.rules_channel_id = 1135973867001753711
+        self.help_and_questions_channel_id = 1397028615446855780
+
+    async def send_welcome_message(self, member, channel):
+        
+        # Send a welcome message in the welcome channel
+        if channel:
+            await channel.send(f"Welcome to {self.server_name}, {member.mention}! "
+                            f"You’re now part of our awesome, chill gaming crew. 🎮 "
+                            f"Drop by <#{self.introduction_channel_id}> and tell us about yourself. We love meeting our squad! "
+                            f"Check <#{self.information_channel_id}> for server details and swing by <#{self.rules_channel_id}> to know what’s up. "
+                            f"Got questions? Hit <#{self.help_and_questions_channel_id}>, and staff or members will help. "
+                            f"If you spot us in voice chat, come say hi. We’d love to get to know you and game together!")
