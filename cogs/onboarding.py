122aebfaa03939859a66a257a3122d86ce30a1a7
Anita Orstad <anita.orstad@gmail.com>
2025-08-04 20:29:22 +0200

    moved log message to after chacking channel

--- cogs/onboarding.py
+++ cogs/onboarding.py
@@ -28,9 +28,9 @@ class Onboarding(commands.Cog):
         if message.author.bot:
             logging.info("Ignoring bot message: %s", message.author.name)
             return
-        logging.info(f"Message received from {message.author.name} in channel {message.channel.name} with id {message.channel.id}")
-        logging.info("%s has roles: %s", message.author.name, [role.name for role in message.author.roles])
         if message.channel.id == self.config['start_here_channel_id']:
+            logging.info(f"Message received from {message.author.name} in channel {message.channel.name} with id {message.channel.id}")
+            logging.info("%s has roles: %s", message.author.name, [role.name for role in message.author.roles])
             manager = OnboardingManager(message.guild)
             welcome_manager = WelcomeMessageManager(message.author)
             await manager.handle_onboarding_message(message.author, message.channel)
